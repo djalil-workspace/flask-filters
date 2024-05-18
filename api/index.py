678e03553b88949fc4b14ca667833deaf8b64589
djalil-workspace <fun.id.edits@gmail.com>
2024-05-18 13:15:09 +0500

    release 2024-05-18 1:15

--- api/index.py
+++ api/index.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+from sqlite3 import connect
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/')
+def index():
+    return '<a href="/api" style="font-size: 35px;">API</a>'
+
+
+@app.route('/api')
+def api():
+    with connect('db.sqlite3') as c:
+        json = []
+        data = c.execute('SELECT * FROM users').fetchall()
+
+        for user in data:
+            json.append({'fullname': user[0], 'age': user[1]})
+
+    return jsonify(json)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
