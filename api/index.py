953f171328ed43ef91785ddb188a27ad48cb43d5
djalil-workspace <fun.id.edits@gmail.com>
2024-06-03 18:43:23 +0500

    release 2024-06-03 6:43

--- api/index.py
+++ api/index.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify, request, render_template, redirect
+from flask import Flask, jsonify, request, redirect
 from datetime import date, timedelta, datetime
 from sqlite3 import connect
 from flask_cors import CORS, cross_origin
 
-
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
 
 
 def get_seq() -> int:
@@ -16,17 +16,22 @@ def get_seq() -> int:
     return int(data[0][0]) + 1
 
 
-@app.route('/', methods=['POST', 'GET'])
-def AddCustomer():
-    try:
+@app.route('/')
+def home():
+    return redirect('/All')
+
+
+@app.route('/post', methods=['POST', 'GET'])
+def add_customer():
+    if request.method == 'POST':
         user_id = get_seq()
 
-        form_fullname = request.form['fullname']
-        form_date = date.fromisoformat(request.form['date'])
-        form_category = request.form['category']
-        form_address = request.form['address']
-        form_mobile = request.form['mobile']
-        form_comment = request.form['comment']
+        form_fullname  =  request.args.get('fullname')
+        form_date      =  date.fromisoformat(request.args.get('date'))
+        form_category  =  request.args.get('category')
+        form_address   =  request.args.get('address')
+        form_mobile    =  request.args.get('mobile')
+        form_comment   =  request.args.get('comment')
 
         filter1 = form_date + timedelta(days=60) if form_category == '2-4 ay' else form_date + timedelta(days=180)
         filter2 = form_date + timedelta(days=120) if form_category == '2-4 ay' else form_date + timedelta(days=180)
@@ -40,10 +45,66 @@ def AddCustomer():
                     )
                 """
             )
-    except Exception as e:
-        print(e)
 
-    return render_template('index.html')
+    return redirect('/All')
+
+
+@app.route('/check/<string:fullname>', methods=['POST', 'GET'])
+def check(fullname):
+    try:
+        with connect('db.sqlite3') as c:
+            user = c.execute(f'SELECT * FROM customers WHERE fullname="{fullname}"').fetchall()[-1]
+            
+            user_id      =  get_seq()
+            filter_date  =  user[2]
+            category     =  user[3]
+            address      =  user[4]
+            mobile       =  user[5]
+            comment      =  user[6]
+            filter_1     =  user[7]
+            filter_2     =  user[8]
+
+            if category == '2-4 ay':
+                if filter_1 == filter_2:
+                    filter_1 = date.fromisoformat(filter_1) + timedelta(days=60)
+                    filter_2 = date.fromisoformat(filter_2) + timedelta(days=120)
+                else:
+                    filter_1 = date.fromisoformat(filter_1) + timedelta(days=60)
+
+            if category == '6 ay':
+                filter_1 = date.fromisoformat(filter_1) + timedelta(days=180)
+                filter_2 = date.fromisoformat(filter_2) + timedelta(days=180)
+
+            c.execute(
+                f'''
+                    INSERT INTO customers VALUES (
+                        {user_id}, "{fullname}", "{filter_date}",
+                        "{category}", "{address}", "{mobile}",
+                        "{comment}", "{filter_1}", "{filter_2}"
+                    )
+                '''
+            )
+
+            print(user)
+            
+            return {'error': None}
+            
+    except Exception as e:
+        return {'exception': f"{e}"}
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    q = request.args.get('q')
+    result = []
+
+    with connect('db.sqlite3') as c:
+        all_users = c.execute('SELECT * FROM customers').fetchall()
+        for user in all_users:
+            if q.lower() in [str(i).lower() for i in user]:
+                result.append(user)
+
+    return jsonify(result)
 
 
 @app.route("/<string:time>")
@@ -51,7 +112,7 @@ def AddCustomer():
 def api(time):
     if time == "All":
         with connect('db.sqlite3') as c:
-            data = c.execute("SELECT * FROM customers").fetchall()
+            data = c.execute("SELECT * FROM customers").fetchall()      
     elif time == "Today":
         with connect('db.sqlite3') as c:
             today = datetime.now().date()
@@ -65,23 +126,41 @@ def api(time):
             past = datetime.now().date()
             data = c.execute("SELECT * FROM customers WHERE filter_1 < ? OR filter_2 < ?", (past, past)).fetchall()
     else:
-        return redirect('/')
+        return redirect('/All')
+
+    users = {user[1] for user in data}
+
+    clean_data = []
+    for user in users:
+        clean_data.append(c.execute(f'SELECT * FROM customers WHERE fullname="{user}"').fetchall()[-1])
 
     api_data = [
         {
-            'id': user[0],
-            'fullname': user[1],
-            'date': user[2],
-            'category': user[3],
-            'address': user[4],
-            'mobile': user[5],
-            'comment': user[6],
-            'filter1': user[7],
-            'filter2': user[8],
-        } for user in data
+            'id'        :  user[0],
+            'fullname'  :  user[1],
+            'date'      :  user[2],
+            'category'  :  user[3],
+            'address'   :  user[4],
+            'mobile'    :  user[5],
+            'comment'   :  user[6],
+            'filter1'   :  user[7],
+            'filter2'   :  user[8],
+        } for user in clean_data
     ]
 
     return jsonify(api_data)
+
+
+@app.route('/delete/<string:fullname>')
+def delete_user(fullname):
+    try:
+        with connect('db.sqlite3') as c:
+            c.execute(f'DELETE FROM customers WHERE fullname="{fullname}"')
+
+        return {'error': None}
+
+    except Exception as e:
+        return {'exception': str(e)}
 
 
 if __name__ == '__main__':
